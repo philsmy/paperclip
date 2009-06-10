a241be58ab5372ac9e84803189cfa4e83816b301
Phil Smy <smyp@phil-smys-mac-pro-2.local>
2009-06-10 12:19:01 +0200

    use thumbnail instead of resize

--- lib/paperclip/thumbnail.rb
+++ lib/paperclip/thumbnail.rb
@@ -61,7 +61,7 @@ module Paperclip
     # into the thumbnail.
     def transformation_command
       scale, crop = @current_geometry.transformation_to(@target_geometry, crop?)
-      trans = "-resize \"#{scale}\""
+      trans = "-thumbnail \"#{scale}\""
       trans << " -crop \"#{crop}\" +repage" if crop
       trans << " #{convert_options}" if convert_options?
       trans
